fc80981764371babcb1c79fb16185fef21fb484a
fanqiexiaohuoguo <huang0616ke@163.com>
2019-08-28 17:34:38 +0800

    添加高亮效果

--- main.js
+++ main.js
@@ -10,6 +10,21 @@ window.onscroll=function(){
     else{
         topNavBarInner.classList.remove("sticky")
     }
+    //添加高亮效果
+    let minIndex=0
+    let specialTags=document.querySelectorAll("[data-highlight]")
+    for(let i=0;i<specialTags.length;i++){
+        if(Math.abs(specialTags[i].offsetTop-window.scrollY)<Math.abs(specialTags[minIndex].offsetTop-window.scrollY)){
+            minIndex=i
+        }
+    }
+    let specialId=specialTags[minIndex].id
+    let li=document.querySelector('a[href="#'+ specialId +'"]').parentNode
+    let brothersAndMe=li.parentNode.children
+    for(let i=0;i<brothersAndMe.length;i++){
+        brothersAndMe[i].classList.remove("highlight")
+    }
+    li.classList.add('highlight')
 }
 //添加次级菜单效果
 let ul=document.getElementsByClassName("menuTrigger")[0]
